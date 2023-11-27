ce094079abdc3c3bc7886b6902cc3f2b2356d0b3
Clement <clement.haon.13@gmail.com>
2023-11-27 19:40:24 +0100

    add response test

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const puppeteer = require('puppeteer');
-const {getPickWithUrl} = require('./getPickOfUrl');
- 
+const { getPickWithUrl } = require('./getPickOfUrl');
+
 const vgmUrl = 'https://winnersandwhiners.com/';
- 
-app.get('/odds', async (req, res ) => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setDefaultNavigationTimeout(0); 
-  await page.goto(vgmUrl);
- 
-  //const textContent = await page.evaluate(() => document.querySelector('div.pick').textContent);
+
+app.get('/odds', async (req, res) => {
+    return res.send('Hello World!');
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    await page.goto(vgmUrl);
+
+    //const textContent = await page.evaluate(() => document.querySelector('div.pick').textContent);
     //console.log(textContent)
     const selector = 'div.game-index-article-buttons > a'
     await page.waitForSelector(selector)
@@ -21,23 +22,23 @@ app.get('/odds', async (req, res ) => {
     let arrayLinkFinal = [];
 
     if (links != undefined && links != null && links.length > 0) {
-        for(let i = 0; i < links.length; i++) {
-            if(links[i].includes(stringRef)) {
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].includes(stringRef)) {
                 arrayLinkFinal.push(links[i]);
             }
         }
     }
     console.log(arrayLinkFinal)
 
-  await browser.close();
+    await browser.close();
 
-  for(let i = 0; i < arrayLinkFinal.length; i++) {
+    for (let i = 0; i < arrayLinkFinal.length; i++) {
 
-    await getPickWithUrl(arrayLinkFinal[i]);
-    
-}
+        await getPickWithUrl(arrayLinkFinal[i]);
+
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
-  });
+});
