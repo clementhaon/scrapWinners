0d62281dfb82ab6fd358c4d6118379254e22cadc
Clement <clement.haon.13@gmail.com>
2023-11-27 19:57:55 +0100

    test puppeteer ubuntu

--- index.js
+++ index.js
@@ -7,35 +7,41 @@ const { getPickWithUrl } = require('./getPickOfUrl');
 const vgmUrl = 'https://winnersandwhiners.com/';
 
 app.get('/odds', async (req, res) => {
-    return res.send('Hello World!');
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto(vgmUrl);
-
-    //const textContent = await page.evaluate(() => document.querySelector('div.pick').textContent);
-    //console.log(textContent)
-    const selector = 'div.game-index-article-buttons > a'
-    await page.waitForSelector(selector)
-    const links = await page.$$eval(selector, am => am.filter(e => e.href).map(e => e.href))
-    const stringRef = "https://winnersandwhiners.com/games";
-    let arrayLinkFinal = [];
-
-    if (links != undefined && links != null && links.length > 0) {
-        for (let i = 0; i < links.length; i++) {
-            if (links[i].includes(stringRef)) {
-                arrayLinkFinal.push(links[i]);
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto(vgmUrl);
+    
+        //const textContent = await page.evaluate(() => document.querySelector('div.pick').textContent);
+        //console.log(textContent)
+        const selector = 'div.game-index-article-buttons > a'
+        await page.waitForSelector(selector)
+        const links = await page.$$eval(selector, am => am.filter(e => e.href).map(e => e.href))
+        const stringRef = "https://winnersandwhiners.com/games";
+        let arrayLinkFinal = [];
+        
+        if (links != undefined && links != null && links.length > 0) {
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].includes(stringRef)) {
+                    arrayLinkFinal.push(links[i]);
+                }
             }
         }
-    }
-    console.log(arrayLinkFinal)
-
-    await browser.close();
-
-    for (let i = 0; i < arrayLinkFinal.length; i++) {
-
-        await getPickWithUrl(arrayLinkFinal[i]);
-
+        console.log(arrayLinkFinal)
+        return res.send(arrayLinkFinal);
+    
+    
+        await browser.close();
+    
+        for (let i = 0; i < arrayLinkFinal.length; i++) {
+    
+            await getPickWithUrl(arrayLinkFinal[i]);
+    
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send(error);
     }
 });
 
